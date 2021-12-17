a0bbf821333822780fde2486ff091460389941b9
marina <marina@marina.lopez>
2021-12-17 11:53:39 +0100

    filter fix 🩺

--- src/components/Plants.vue
+++ src/components/Plants.vue
@@ -38,14 +38,7 @@
           ></template>
         </button> -->
         <label for="buscar" class="hide">Buscar</label>
-        <input
-          type="text"
-          id="buscar"
-          v-model="buscar"
-          class="form-control"
-          placeholder="Ejemplo: Monstera"
-          @keyup="searchPlants($event)"
-        />
+        <input type="text" id="buscar" v-model="buscar" class="form-control" placeholder="Ejemplo: Monstera" @keyup="searchPlants($event)" />
       </div>
     </div>
     <!-- SIDEBAR FILTERS -->
@@ -60,39 +53,19 @@
         <div class="row normal">
           <label class="form-control">
             Baja
-            <input
-              v-model="form.dedication"
-              type="checkbox"
-              id="baja"
-              name="Baja"
-              value="Baja"
-              @change="updateList($event)"
-            />
+            <input v-model="form.dedication" type="checkbox" id="baja" name="Baja" value="Baja" @change="updateList($event)" />
             <span class="checkmark"></span>
           </label>
 
           <label class="form-control">
             Media
-            <input
-              v-model="form.dedication"
-              type="checkbox"
-              id="media"
-              name="Media"
-              value="Media"
-              @change="updateList($event)" />
+            <input v-model="form.dedication" type="checkbox" id="media" name="Media" value="Media" @change="updateList($event)" />
             <span class="checkmark"></span
           ></label>
 
           <label class="form-control">
             Alta
-            <input
-              v-model="form.dedication"
-              type="checkbox"
-              id="alta"
-              name="Alta"
-              value="Alta"
-              @change="updateList($event)"
-            />
+            <input v-model="form.dedication" type="checkbox" id="alta" name="Alta" value="Alta" @change="updateList($event)" />
             <span class="checkmark"></span>
           </label>
         </div>
@@ -101,26 +74,13 @@
         <div class="row normal">
           <label class="form-control">
             Exterior
-            <input
-              v-model="form.location"
-              type="checkbox"
-              id="exterior"
-              name="exterior"
-              value="exterior"
-              @change="updateList($event)"
-            />
+            <input v-model="form.location" type="checkbox" id="exterior" name="exterior" value="exterior" @change="updateList($event)" />
             <span class="checkmark"></span>
           </label>
 
           <label class="form-control">
             Interior
-            <input
-              v-model="form.location"
-              type="checkbox"
-              id="interior"
-              name="interior"
-              value="interior"
-              @change="updateList($event)" />
+            <input v-model="form.location" type="checkbox" id="interior" name="interior" value="interior" @change="updateList($event)" />
             <span class="checkmark"></span
           ></label>
         </div>
@@ -130,26 +90,13 @@
         <div class="row normal">
           <label class="form-control">
             Directa
-            <input
-              v-model="form.light"
-              type="checkbox"
-              id="directa"
-              name="directa"
-              value="directa"
-              @change="updateList($event)"
-            />
+            <input v-model="form.light" type="checkbox" id="directa" name="directa" value="directa" @change="updateList($event)" />
             <span class="checkmark"></span>
           </label>
 
           <label class="form-control">
             Indirecta
-            <input
-              v-model="form.light"
-              type="checkbox"
-              id="indirecta"
-              name="indirecta"
-              value="indirecta"
-              @change="updateList($event)" />
+            <input v-model="form.light" type="checkbox" id="indirecta" name="indirecta" value="indirecta" @change="updateList($event)" />
             <span class="checkmark"></span
           ></label>
         </div>
@@ -157,14 +104,7 @@
         <h4>Segura para:</h4>
         <div class="row animals">
           <label class="form-control">
-            <input
-              v-model="form.toxicity"
-              type="checkbox"
-              id="huey"
-              name="dog"
-              value="dog"
-              @change="updateList($event)"
-            />
+            <input v-model="form.toxicity" type="checkbox" id="huey" name="dog" value="dog" @change="updateList($event)" />
             <span class="buttontype">
               <img width="18" height="18" src="../assets/svg/animals_dog.svg" />
               Perretes</span
@@ -174,59 +114,21 @@
           </label>
 
           <label class="form-control">
-            <input
-              v-model="form.toxicity"
-              type="checkbox"
-              id="cat"
-              name="cat"
-              value="cat"
-              @change="updateList($event)" />
-            <span class="buttontype"
-              ><img
-                width="18"
-                height="18"
-                src="../assets/svg/animals_cat.svg"
-              />Gaticos</span
-            >
+            <input v-model="form.toxicity" type="checkbox" id="cat" name="cat" value="cat" @change="updateList($event)" />
+            <span class="buttontype"><img width="18" height="18" src="../assets/svg/animals_cat.svg" />Gaticos</span>
 
             <span class="checkmark"></span
           ></label>
 
           <label class="form-control">
-            <input
-              v-model="form.toxicity"
-              type="checkbox"
-              id="horse"
-              name="horse"
-              value="horse"
-              @change="updateList($event)"
-            />
-            <span class="buttontype"
-              ><img
-                width="18"
-                height="18"
-                src="../assets/svg/horse-2.svg"
-              />Caballos</span
-            >
+            <input v-model="form.toxicity" type="checkbox" id="horse" name="horse" value="horse" @change="updateList($event)" />
+            <span class="buttontype"><img width="18" height="18" src="../assets/svg/horse-2.svg" />Caballos</span>
 
             <span class="checkmark"></span>
           </label>
           <label class="form-control">
-            <input
-              v-model="form.toxicity"
-              type="checkbox"
-              id="rabbit"
-              name="rabbit"
-              value="rabbit"
-              @change="updateList($event)"
-            />
-            <span class="buttontype"
-              ><img
-                width="18"
-                height="18"
-                src="../assets/svg/animals_rabbit_1.svg"
-              />Conejos
-            </span>
+            <input v-model="form.toxicity" type="checkbox" id="rabbit" name="rabbit" value="rabbit" @change="updateList($event)" />
+            <span class="buttontype"><img width="18" height="18" src="../assets/svg/animals_rabbit_1.svg" />Conejos </span>
             <span class="checkmark"></span>
           </label>
         </div>
@@ -246,11 +148,7 @@
         <i class="fa fa-search"></i> -->
       </div>
       <div class="row list left">
-        <div
-          class="noresults"
-          style="text-align: center"
-          v-if="items.length === 0"
-        >
+        <div class="noresults" style="text-align: center" v-if="items.length === 0">
           <h2>No encontramos nada con ese nombre</h2>
           <p class="big">Por favor, deja de inventarte nombres de plantas</p>
           <img src="https://c.tenor.com/IzXxm7sO-HwAAAAd/nothing-to.gif" />
@@ -260,23 +158,14 @@
             <router-link
               :to="{
                 name: 'Plants',
-                params: { plantslug: item.slug, plant: JSON.stringify(item) },
+                params: { plantslug: item.slug, plant: JSON.stringify(item) }
               }"
             >
               <template v-if="!item.image.length">
-                <img
-                  class="card-img"
-                  src="../assets/no-photo.jpg"
-                  title="no image"
-                  alt="no image"
-                />
+                <img class="card-img" src="../assets/no-photo.jpg" title="no image" alt="no image" />
               </template>
               <template v-else>
-                <img
-                  class="card-img"
-                  v-bind:src="item.image"
-                  v-bind:alt="item.scientificName"
-                />
+                <img class="card-img" v-bind:src="item.image" v-bind:alt="item.scientificName" />
               </template>
             </router-link>
 
@@ -284,7 +173,7 @@
               <router-link
                 :to="{
                   name: 'Plants',
-                  params: { plantslug: item.slug, plant: JSON.stringify(item) },
+                  params: { plantslug: item.slug, plant: JSON.stringify(item) }
                 }"
               >
                 <h3 class="card__title">{{ item.name }}</h3>
@@ -307,9 +196,7 @@
                   class="toxicity"
                   src="../assets/svg/animals_dog.svg"
                   v-bind:class="{ nontoxic: item.toxicity.dog.value == 'No' }"
-                  v-bind:title="
-                    item.toxicity.dog.value + ' tóxica para perretes'
-                  "
+                  v-bind:title="item.toxicity.dog.value + ' tóxica para perretes'"
                   v-bind:alt="item.toxicity.dog.value"
                   :key="item.key"
                 />
@@ -318,9 +205,7 @@
                   class="toxicity"
                   src="../assets/svg/animals_cat.svg"
                   v-bind:class="{ nontoxic: item.toxicity.cat.value == 'No' }"
-                  v-bind:title="
-                    item.toxicity.cat.value + ' tóxica para gatetes'
-                  "
+                  v-bind:title="item.toxicity.cat.value + ' tóxica para gatetes'"
                   v-bind:alt="item.toxicity.cat.value"
                   :key="item.key"
                 />
@@ -329,9 +214,7 @@
                   class="toxicity"
                   src="../assets/svg/horse-2.svg"
                   v-bind:class="{ nontoxic: item.toxicity.horse.value == 'No' }"
-                  v-bind:title="
-                    item.toxicity.horse.value + ' tóxica para caballos'
-                  "
+                  v-bind:title="item.toxicity.horse.value + ' tóxica para caballos'"
                   v-bind:alt="item.toxicity.horse.value"
                   :key="item.key"
                 />
@@ -340,11 +223,9 @@
                   class="toxicity"
                   src="../assets/svg/animals_rabbit_1.svg"
                   v-bind:class="{
-                    nontoxic: item.toxicity.rabbit.value == 'No',
+                    nontoxic: item.toxicity.rabbit.value == 'No'
                   }"
-                  v-bind:title="
-                    item.toxicity.rabbit.value + ' tóxica para conejos'
-                  "
+                  v-bind:title="item.toxicity.rabbit.value + ' tóxica para conejos'"
                   v-bind:alt="item.toxicity.rabbit.value"
                   :key="item.key"
                 />
@@ -372,157 +253,139 @@
 </template>
 
 <script>
-//import datos from "../assets/json/data.json";
-import datos from "../assets/json/plants.json";
+  //import datos from "../assets/json/data.json";
+  import datos from "../assets/json/plants.json";
 
-export default {
-  // Exporto mi componente 'Plants'
-  name: "PlantsList",
-  props: {
-    msg: String,
-  },
+  export default {
+    // Exporto mi componente 'Plants'
+    name: "PlantsList",
+    props: {
+      msg: String
+    },
 
-  // declaro el elemento buscar
-  data() {
-    return {
-      ascending: true,
-      buscar: "",
-      form: {
-        dedication: [],
-        location: [],
-        light: [],
-        toxicity: [],
-      },
-      items: datos,
-      /*       checkedLocation: true, */
-    };
-  },
+    // declaro el elemento buscar
+    data() {
+      return {
+        ascending: true,
+        buscar: "",
+        form: {
+          dedication: [],
+          location: [],
+          light: [],
+          toxicity: []
+        },
+        items: datos
+        /*       checkedLocation: true, */
+      };
+    },
 
-  // Creo un método llamado 'items' y obtengo los datos de las Plants
-  // Asimismo filtro la búsqueda de las Plants con el método filter()
-  // buscador inicial funcionando:
-  /* */
-  /* probar la opcioń de meter más filtros via una variable como este ejempo  https://medium.com/swlh/filtering-sorting-and-searching-in-arrays-with-vue-js-f60951c040fc*/
-  methods: {
-    searchPlants() {
-      let tempPlantas;
+    // Creo un método llamado 'items' y obtengo los datos de las Plants
+    // Asimismo filtro la búsqueda de las Plants con el método filter()
+    // buscador inicial funcionando:
+    /* */
+    /* probar la opcioń de meter más filtros via una variable como este ejempo  https://medium.com/swlh/filtering-sorting-and-searching-in-arrays-with-vue-js-f60951c040fc*/
+    methods: {
+      searchPlants() {
+        let tempPlantas;
 
-      tempPlantas = datos.filter((item) => {
-        return (
-          item.scientificName
-            .toLowerCase()
-            .includes(this.buscar.toLowerCase()) ||
-          item.name.toLowerCase().includes(this.buscar.toLowerCase())
-        );
-      });
-      // Show sorted array in descending or ascending order
-      if (!this.ascending) {
-        tempPlantas.reverse();
-      }
+        tempPlantas = datos.filter((item) => {
+          return item.scientificName.toLowerCase().includes(this.buscar.toLowerCase()) || item.name.toLowerCase().includes(this.buscar.toLowerCase());
+        });
+        // Show sorted array in descending or ascending order
+        if (!this.ascending) {
+          tempPlantas.reverse();
+        }
 
-      /*       if (!this.checked.location) {
+        /*       if (!this.checked.location) {
         tempPlantas.filter((item) =>
           this.checked.location.includes(item.care.location)
         );
       } */
 
-      this.items = tempPlantas;
-    },
-    updateList() {
-      if (
-        !this.form.dedication.length &&
-        !this.form.location.length &&
-        !this.form.light.length &&
-        !this.form.toxicity.length
-      ) {
-        this.items = datos;
-      } else {
-        this.items = datos.filter((it) => {
-          // console.log("dedication", this.form.dedication.length ? this.form.dedication.includes(it.care.dedication.value) : true);
-          // console.log(
-          //   "location",
-          //   this.form.location.length
-          //     ? this.form.location.some((loc) => {
-          //         return it.care.location.some((j) => j.toLowerCase() === loc);
-          //       })
-          //     : true
-          // );
-          if (
-            this.form.dedication.length
-              ? this.form.dedication.includes(it.care.dedication.value)
-              : true && this.form.location.length
-              ? this.form.location.some((loc) => {
-                  return it.care.location.some((j) => j.toLowerCase() === loc);
-                })
-              : true && this.form.light.length
-              ? this.form.light.includes(it.care.light.value.toLowerCase())
-              : true && this.form.toxicity.length
-              ? this.plantIsSafe(it)
-              : true
-          ) {
-            return it;
-          }
-        });
-      }
-    },
-    plantIsSafe(plant) {
-      if (!plant.toxicity) {
-        return true;
-      } else {
-        const elements = Object.keys(plant.toxicity);
-        const nonToxicFor = [];
-        elements.forEach((it) => {
-          if (plant.toxicity[it] && plant.toxicity[it].value) {
-            plant.toxicity[it].value.toLowerCase() === "no" &&
+        this.items = tempPlantas;
+      },
+      updateList() {
+        if (!this.form.dedication.length && !this.form.location.length && !this.form.light.length && !this.form.toxicity.length) {
+          this.items = datos;
+        } else {
+          this.items = datos.filter((it) => {
+            if (this.plantIsValid(it)) {
+              return it;
+            }
+          });
+        }
+      },
+      plantIsValid(plant) {
+        const validation = [];
+        validation.push(this.form.dedication.length ? this.form.dedication.includes(plant.care.dedication.value) : true);
+        validation.push(
+          this.form.location.length
+            ? this.form.location.some((loc) => {
+                return plant.care.location.some((j) => j.toLowerCase() === loc);
+              })
+            : true
+        );
+        validation.push(this.form.light.length ? this.form.light.includes(plant.care.light.value.toLowerCase()) : true);
+        validation.push(this.form.toxicity.length ? this.plantIsSafe(plant) : true);
+        return validation.every((it) => it === true);
+      },
+      plantIsSafe(plant) {
+        if (!plant.toxicity) {
+          return true;
+        } else {
+          const elements = Object.keys(plant.toxicity);
+          const nonToxicFor = [];
+          elements.forEach((it) => {
+            if (plant.toxicity[it] && plant.toxicity[it].value) {
+              plant.toxicity[it].value.toLowerCase() === "no" && nonToxicFor.push(it);
+            } else {
               nonToxicFor.push(it);
-          } else {
-            nonToxicFor.push(it);
-          }
-        });
-        return this.form.toxicity.every((it) => nonToxicFor.includes(it));
+            }
+          });
+          return this.form.toxicity.every((it) => nonToxicFor.includes(it));
+        }
       }
-    },
-  },
-};
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
-.sidebar {
-  flex-basis: 20%;
-  padding-right: 1rem;
-  .row {
-    margin-bottom: 2rem;
+  .sidebar {
+    flex-basis: 20%;
+    padding-right: 1rem;
+    .row {
+      margin-bottom: 2rem;
+    }
+    h4 {
+      font-weight: 600;
+    }
   }
-  h4 {
-    font-weight: 600;
+  .row.list {
+    flex-basis: 78%;
+    gap: 3%;
   }
-}
-.row.list {
-  flex-basis: 78%;
-  gap: 3%;
-}
-.sort-button {
-  margin-bottom: 0;
-}
-p.notoxica {
-  line-height: 1.8;
-  color: #797979;
-}
-.row-search {
-  margin-bottom: 0;
-}
-.row.list {
-  flex-basis: 78%;
-  gap: 2rem 3%;
-}
-.sort-button {
-  margin-bottom: 0;
-}
-p.notoxica {
-  line-height: 1.8;
-  color: #797979;
-}
-.row-search {
-  margin-bottom: 0;
-}
+  .sort-button {
+    margin-bottom: 0;
+  }
+  p.notoxica {
+    line-height: 1.8;
+    color: #797979;
+  }
+  .row-search {
+    margin-bottom: 0;
+  }
+  .row.list {
+    flex-basis: 78%;
+    gap: 2rem 3%;
+  }
+  .sort-button {
+    margin-bottom: 0;
+  }
+  p.notoxica {
+    line-height: 1.8;
+    color: #797979;
+  }
+  .row-search {
+    margin-bottom: 0;
+  }
 </style>
